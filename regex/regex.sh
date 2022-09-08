2e16dc3bdaad9a47c52c6f899910b6c076379a2b
Daniel Albright <albri118@morris.umn.edu>
2022-09-08 15:52:45 -0500

    attempt second regex

--- regex/regex.sh
+++ regex/regex.sh
@@ -2,4 +2,4 @@
 
 awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r0_input.txt > r0_output.txt
 
-awk 'match() {print}' <r1_input.txt > r1_output.txt
+awk 'match($0, /([KNV][Ki]\w?c?e?n?t?), (\b[tah][uva][rom]\w?\w?\w?\w?)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r1_input.txt > r1_output.txt
